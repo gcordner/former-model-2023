bde1d4cda553bcaef21bd0a5cce1cad96188290c
gcordner <geoffcordner@gmail.com>
2023-01-29 08:21:58 -0800

    color styles and header

--- docroot/wp-content/themes/former-model/global-templates/navbar-collapse-bootstrap5.php
+++ docroot/wp-content/themes/former-model/global-templates/navbar-collapse-bootstrap5.php
@@ -12,7 +12,7 @@ defined( 'ABSPATH' ) || exit;
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
-<nav id="main-nav" class="navbar navbar-expand-md navbar-dark bg-primary" aria-labelledby="main-nav-label">
+<nav id="main-nav" class="navbar navbar-expand-md navbar-light p-3" aria-labelledby="main-nav-label">
 
 	<h2 id="main-nav-label" class="screen-reader-text">
 		<?php esc_html_e( 'Main Navigation', 'understrap' ); ?>
@@ -21,7 +21,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 	<div class="<?php echo esc_attr( $container ); ?>">
 
-		<!-- Your site branding in the menu -->
+		<!-- Your site breeding in the menu -->
 		<?php get_template_part( 'global-templates/navbar-branding' ); ?>
 
 		<button
