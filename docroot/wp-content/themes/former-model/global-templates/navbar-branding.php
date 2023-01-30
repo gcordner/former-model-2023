ba538496563f9f6866544062850962bb978a8874
gcordner <geoffcordner@gmail.com>
2023-01-30 09:33:33 -0800

    more branding styles

--- docroot/wp-content/themes/former-model/global-templates/navbar-branding.php
+++ docroot/wp-content/themes/former-model/global-templates/navbar-branding.php
@@ -13,7 +13,7 @@ if ( ! has_custom_logo() ) { ?>
 
 	<?php if ( is_front_page() && is_home() ) : ?>
 
-		<h1 class="navbar-brand mb-0">
+		<h1 class="navbar-brand mb-0 h1-mimic">
 			<a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url">
 				<?php bloginfo( 'name' ); ?>
 			</a>
@@ -21,7 +21,7 @@ if ( ! has_custom_logo() ) { ?>
 
 	<?php else : ?>
 
-		<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url">
+		<a class="navbar-brand h1-mimic" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url">
 			<?php bloginfo( 'name' ); ?>
 		</a>
 
